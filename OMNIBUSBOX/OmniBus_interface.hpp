8a2b74a93ee60673d3687a8b20207b93f25ed148
mor <mor@front.ru>
2014-02-25 12:55:59 +0300

    Модифицирована заглушка OmniBus для тестирования

---
+++ OMNIBUSBOX/OmniBus_interface.hpp
@@ -0,0 +1,21 @@
+#ifndef _OMNIBUS_INT_HPP_
+#define _OMNIBUS_INT_HPP_
+
+#include <BTI1553.h>
+#define _WIN32
+#include <windows.h>
+
+class OmniDriver_interface
+{
+  public:
+    virtual BOOL ListMultiBlockRd(LPUSHORT buf, LPINT blkcountptr, LISTADDR listaddr, HCORE handleval)
+    {
+      return 0;
+    }
+	virtual void MsgDataWr(LPUSHORT buf,INT count, MSGADDR msgaddr, HCORE handleval)
+	{
+	}
+	virtual ~OmniDriver_interface(){}
+};
+
+#endif
