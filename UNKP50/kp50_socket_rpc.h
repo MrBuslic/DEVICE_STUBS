faf5a33461baea5b03a38ad4deebe4abd73213c9
oleg <okalinin@mail.ru>
2018-03-14 14:20:56 +0300

    kp50 заготовка

---
+++ UNKP50/kp50_socket_rpc.h
@@ -0,0 +1,160 @@
+#ifndef FOI_SOCKET_RPC_H
+#define FOI_SOCKET_RPC_H
+
+#include <QObject>
+#include <QString>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include "rpc_foi.h"
+
+#include <QWidget>
+#include <QTextEdit>
+#include <QWaitCondition>
+
+#include "rpc_loger.h"
+struct SignalData
+{
+	SignalData() : signal_needed(false) {}
+	SignalData(const SignalData&) {}
+	QWaitCondition cond;
+	QMutex mutex;
+	QVariantList signal_data;
+	bool signal_needed;
+};
+class Socket_RPC_SIGNAL_Object : public QObject
+{
+	Q_OBJECT
+public:
+	Socket_RPC_SIGNAL_Object();
+	~Socket_RPC_SIGNAL_Object()
+	{
+	}
+	void set_app(RpcFoiWidget* _app);
+
+	void set_socket(QTcpSocket* _rpc_socket);
+signals:
+	void send_signal(QByteArray* _arr);
+public slots:
+	void foi_interrupt(int _n, short _chan, double _u, double _t);
+
+	void send_signal_slot(QByteArray* _arr);
+	void read_data();
+	void sock_error(QAbstractSocket::SocketError _err);
+private:
+	void send_signal_func(QByteArray* _arr);
+	QTcpSocket* rpc_socket;
+	QMutex signal_mutex;
+	RpcFoiWidget* app;
+	static int obj_num;
+	QMap<QString, std::shared_ptr<SignalData> > data_map;
+};
+
+
+class Socket_RPC_SIGNAL_Server : public QObject
+{
+	Q_OBJECT
+public:
+	Socket_RPC_SIGNAL_Server(QString _conn_ip, int _conn_port);
+	void set_app(RpcFoiWidget* _app)
+	{
+		app = _app;
+	}
+public slots:
+	void tcp_slot();
+private:
+	QTcpServer* rpc_server;
+	RpcFoiWidget* app;
+	QList<std::shared_ptr<Socket_RPC_SIGNAL_Object> > rpc_objects;
+};
+
+class Socket_RPC_SIGNAL_Thread : public QThread
+{
+	Q_OBJECT
+public:
+	Socket_RPC_SIGNAL_Thread();
+	void set_app(RpcFoiWidget* _app)
+	{
+		app = _app;
+	}
+	void set_params(QString _conn_ip, int _conn_port)
+	{
+		conn_ip = _conn_ip;
+		conn_port = _conn_port;
+	}
+	void run();
+private:
+	Socket_RPC_SIGNAL_Server* rpc_srv;
+	RpcFoiWidget* app;
+	QString conn_ip;
+	int conn_port;
+};
+
+class Socket_RPC_SLOT_Object : public QObject
+{
+	Q_OBJECT
+public:
+	Socket_RPC_SLOT_Object();
+	~Socket_RPC_SLOT_Object()
+	{
+	}
+	void set_app(RpcFoiWidget* _app);
+	void set_socket(QTcpSocket* _rpc_socket);
+	typedef QVariant (Socket_RPC_SLOT_Object::*OPERATOR_EXECUTOR)(QVariantList&);
+	typedef QMap<QString, OPERATOR_EXECUTOR> OPERATORS_MAP;
+public:
+	QVariant QuerySlots(QVariantList& _values);
+	QVariant auto_scroll_clicked(QVariantList& _values);
+	QVariant log_timer_ontimer(QVariantList& _values);
+	QVariant unfoi_chan_setup(QVariantList& _values);
+	QVariant unfoi_run(QVariantList& _values);
+public slots:
+	void read_data();
+	void sock_error(QAbstractSocket::SocketError _err);
+private:
+	OPERATORS_MAP operators_map;
+	QTcpSocket* rpc_socket;
+	RpcFoiWidget* app;
+	bool with_return;
+	static int obj_num;
+};
+
+class Socket_RPC_SLOT_Server : public QObject
+{
+	Q_OBJECT
+public:
+	Socket_RPC_SLOT_Server(QString _conn_ip, int _conn_port);
+	void set_app(RpcFoiWidget* _app)
+	{
+		app = _app;
+	}
+public slots:
+	void tcp_slot();
+private:
+	QTcpServer* rpc_server;
+	RpcFoiWidget* app;
+	QList<std::shared_ptr<Socket_RPC_SLOT_Object> > rpc_objects;
+};
+
+class Socket_RPC_SLOT_Thread : public QThread
+{
+	Q_OBJECT
+public:
+	Socket_RPC_SLOT_Thread();
+	void set_app(RpcFoiWidget* _app)
+	{
+		app = _app;
+	}
+	void set_params(QString _conn_ip, int _conn_port)
+	{
+		conn_ip = _conn_ip;
+		conn_port = _conn_port;
+	}
+	void run();
+private:
+	Socket_RPC_SLOT_Server* rpc_srv;
+	RpcFoiWidget* app;
+	QString conn_ip;
+	int conn_port;
+
+};
+#endif
