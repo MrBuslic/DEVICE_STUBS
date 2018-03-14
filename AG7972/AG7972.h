f8be8fa32e9b72c392d74461be38ed12d36292c3
oleg <okalinin@mail.ru>
2018-03-14 16:02:13 +0300

    заготовка источника

---
+++ AG7972/AG7972.h
@@ -0,0 +1,53 @@
+#ifndef AG7972_H
+#define AG7972_H
+#ifdef WIN32
+#include "winsock2.h"
+#endif
+#include <QObject>
+#include <QString>
+#include <QWidget>
+#include <QThread>
+#include <QTextEdit>
+#include <QScrollBar>
+#include <QCheckBox>
+#include <QTimer>
+#include <QMutex>
+#include <QLineEdit>
+#include <QLabel>
+#include <QTcpSocket>
+#include <QTcpServer>
+#include <memory>
+#include <qlayout.h>
+#include <loki/Singleton.h>
+
+#define SINGLETON_DEF(x) typedef Loki::SingletonHolder<x,Loki::CreateUsingNew,Loki::NoDestroy> S##x;
+
+class AG7972Widget : public QWidget
+{
+	Q_OBJECT
+public:
+	AG7972Widget();
+public slots:
+	void tcp_slot();
+	void read_data();
+private:
+	QTcpServer* server;
+	QTcpSocket* socket;
+
+	QLineEdit* R_edit;
+	QLabel* u_label;
+	QLabel* i_label;
+	QLabel* state_label;
+	QLabel* u_meas_label;
+	QLabel* i_meas_label;
+
+	double u;
+	double i;
+	bool state;
+
+	void update_graphics();
+signals:
+	void power_out(int n, double _u);
+};
+
+#endif //AG7972_H
