a5046157083d96607b2f1f73ba97047bf5a400d5
a_illarionov <Illarionov.ant@gmail.com>
2019-03-20 14:20:05 +0300

    Имитатор ЦБК

---
+++ cbk_imitator/main.cpp
@@ -0,0 +1,20 @@
+﻿#include "cbk_imitator.h"
+#include <QTextCodec>
+#include <socket_rpc.h>
+
+#ifdef WIN32
+int __stdcall WinMain(HINSTANCE, HINSTANCE, LPSTR cmdLine, int)
+{
+	int argc = __argc;
+	char** argv = __argv;
+
+#else
+int main(int argc, char **argv)
+{
+#endif
+	QTextCodec::setCodecForLocale(QTextCodec::codecForName("utf-8"));
+	QApplication app(argc, argv);
+	CBK_MainWindow CBK;
+	CBK.show();
+	return app.exec();
+}
