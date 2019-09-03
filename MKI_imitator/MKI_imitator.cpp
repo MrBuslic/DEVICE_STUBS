225285dd940a60090a6002d2a0ab7f20606446ac
Timon <NeR_92@mail.ru>
2019-09-03 10:59:44 +0300

    Заполнил поля Reserve заголовка КРО

--- MKI_imitator/MKI_imitator.cpp
+++ MKI_imitator/MKI_imitator.cpp
@@ -22,22 +22,24 @@ MKI_imitator::MKI_imitator()
 	gridLayout->addWidget(rezhim_label, 1, 0);
 	gridLayout->addWidget(ip_label, 2, 0);
 	gridLayout->addWidget(RPIK_label, 3, 0);
-	gridLayout->addWidget(log_edit, 4, 0, 1, 2);
+	gridLayout->addWidget(log_edit, 5, 0, 1, 2);
 
-
+	btn = new QPushButton("server to MKI", widg);
+	gridLayout->addWidget(btn, 4, 0);
+	connect(btn, &QPushButton::clicked, this, &MKI_imitator::writeByBtn);
 	
 	QSettings tmp_settings("Cometa", "СПО МКПА МЦА");
 	QString ip = tmp_settings.value("IP_rm", "192.168.0.100").toString();
 	if (ip == "localhost")	// QUdpSocket::bind не жрёт "localhost". Ему "127.0.0.1" подавай.
 		ip = "127.0.0.1";
-	bool t = _sock.bind(QHostAddress(ip), 10001);
 
-	serv_sock.listen(QHostAddress("127.0.0.1"), 30302);
+	tcpServer = new QTcpServer(widg);
+	connect(tcpServer, SIGNAL(newConnection()), this, SLOT(newConn()));
+	tcpServer->listen(QHostAddress::Any, 33333);
 
+	sock.connectToHost("10.44.4.13", 33333);
+	connect(&sock, &QTcpSocket::readyRead, this, &MKI_imitator::readByBtn);
 
-	connect(&_sock, SIGNAL(error(QAbstractSocket::SocketError)), this, SLOT(error_Slot(QAbstractSocket::SocketError)));
-	connect(&_sock, &QUdpSocket::readyRead, this, &MKI_imitator::read);
-	connect(&serv_sock, &QTcpServer::newConnection, this, &MKI_imitator::connect_ag);
 	
 	frame_slot_thr.set_connection_params("127.0.0.1", FRAME_SLOT);
 	frame_slot_thr.start();
@@ -53,6 +55,63 @@ MKI_imitator::MKI_imitator()
 	QSettings settings(QApplication::applicationDirPath() + "/positions.ini", QSettings::IniFormat);
 	restoreGeometry(settings.value("rmmbk07_geometry").toByteArray());
 	//connect(_sock, SIGNAL(readyRead()), SLOT(read()));
+	RUValue = MU;
+	RRValue = RR;
+	RPIKValue = OFF;
+}
+
+void MKI_imitator::log_msg(QString msg)
+{
+	log_edit->append(msg);
+}
+
+QByteArray MKI_imitator::createCommandHeader(const int & command, const int & dataSize, int id)
+{
+	QByteArray header;
+
+	header.resize(msgHeaderLen);
+	header.fill(0);
+
+	//MsgTypeC
+	numberIntoBA(header, command, header_MsgTypeC_ind);
+
+	//PVC
+	header[0] = BYTE(0x55); header[1] = BYTE(0xAA);	header[2] = BYTE(0x01); header[3] = BYTE(0x00);
+	header[header_addr_receiver_ind] = BYTE(0x02);//Адреса получателя 
+	header[header_addr_sender_ind] = BYTE(0x01);//Адреса отправителя
+
+	numberIntoBA(header, ++MsgRegN, header_msg_reg_n_ind);//MsgRegN
+
+														  //Time
+	unsigned long rcv_t_sec = numberFromBA<quint32>(header, header_sec_time_ind, 4);
+	unsigned long rcv_t_usec = numberFromBA<quint32>(header, header_usec_time_ind, 4);
+
+	my_timer.StopCounter();
+	unsigned long send_sec = rcv_t_sec + my_timer.GetSec();
+	unsigned long send_usec = rcv_t_usec + my_timer.GetUSec();
+
+	numberIntoBA(header, send_sec, header_sec_time_ind);
+	numberIntoBA(header, send_usec, header_usec_time_ind);
+	//KRO_TIME_OUT
+	const int max_wait_sec = 5;
+	numberIntoBA(header, send_usec + max_wait_sec * 1000000, header_krp_timeout_ind);
+	numberIntoBA(header, send_sec + max_wait_sec, header_kro_timeout_ind);
+	//LinkId
+	numberIntoBA(header, id, header_LinkID_ind);//сокет
+												//LinkStat
+	numberIntoBA(header, 0x10, header_linkStat_ind);
+	//DataSize
+	numberIntoBA(header, dataSize, header_datasize_ind);
+
+	numberIntoBA(header, RUValue, header_RU);
+	numberIntoBA(header, RRValue, header_RR);
+	numberIntoBA(header, RPIKValue, header_RPIK);
+
+	//CRC32
+	//unsigned int crc32_new = sp_getCRC32((unsigned char*)(header.data()), header.size() - 4);
+	//numberIntoBA(header, crc32_new, header_crc_header_ind);
+
+	return header;
 }
 
 void MKI_imitator::closeEvent(QCloseEvent *event)
@@ -76,33 +135,12 @@ MKI_imitator::~MKI_imitator()
 
 void MKI_imitator::error_Slot(QAbstractSocket::SocketError socketError)
 {
-	QString tmp = _sock.errorString();
+	log_msg(clientSocket->errorString());
 }
 
 
 void MKI_imitator::read()
 {
-
-}
-
-void MKI_imitator::connect_ag()
-{
-	if (_ag_sock)
-		if((_ag_sock->state() == QAbstractSocket::ConnectedState))
-		{
-			_ag_sock->close();
-			delete _ag_sock;
-			_ag_sock = nullptr;
-		}
-	_ag_sock = serv_sock.nextPendingConnection();
-	connect(_ag_sock, &QTcpSocket::readyRead, this, &MKI_imitator::read_ag);
-	//char c = '\x01';
-	//_ag_sock->write(&c);
-}
-
-void MKI_imitator::read_ag()
-{
-
 
 }
 
@@ -112,4 +150,67 @@ void MKI_imitator::new_frame_07(QString mode_in, int psp_in, int lit_in, int _fm
 	frame_slot_thr.get_frame_bus_obj()->make_new_frame_rm07(mode_in, frame_data);
 
 	return;
+}
+
+void MKI_imitator::newConn()
+{
+	clientSocket = tcpServer->nextPendingConnection();
+	QHostAddress adrv4(clientSocket->peerAddress().toIPv4Address());
+	log_msg(QString("Новое подключение с %1:%2").arg(adrv4.toString()).arg(QString::number(clientSocket->peerPort())));
+	//connect(clientSocket, SIGNAL(readyRead()), this, SLOT(slotReadClient()));
+	connect(clientSocket, &QTcpSocket::readyRead, this, &MKI_imitator::slotReadClient);
+}
+
+void MKI_imitator::slotReadClient()
+{
+	QByteArray read_data;
+	QString ret_str;
+
+	while (clientSocket->bytesAvailable())
+		read_data += clientSocket->read(clientSocket->bytesAvailable());
+	
+	qDebug() << read_data.toHex();
+	qDebug() << read_data.size();
+	log_msg("0x" + QString(read_data.toHex()));
+
+	
+	unsigned int MsgTypeC = numberFromBA<unsigned int>(read_data, 12);	// Код типа сообщения
+	MsgTypeC += 1e9;
+	
+	QByteArray response_data = createCommandHeader(MsgTypeC, 0);
+
+	//response_data.append(read_data.mid(0, 4));	// Код начала заголовка и версии протокола.
+	//response_data.append(read_data.mid(8, 4));	// FromAddr
+	//response_data.append(read_data.mid(4, 4));	// ToAddr
+
+	//numberIntoBA(response_data, MsgTypeC, 12);
+
+	//response_data.append(read_data.mid(16, 14));
+
+	//unsigned int ErrStat = 0;
+	//numberIntoBA(response_data, ErrStat, 40);
+
+	//response_data.append(read_data.mid(44));
+	//response_data.resize(96);
+
+
+	qDebug() << response_data.toHex();
+	qDebug() << response_data.size();
+	clientSocket->write(response_data);
+	//clientSocket->write("ewq");
+}
+
+void MKI_imitator::writeByBtn()
+{
+	sock.write("qwe");
+}
+
+void MKI_imitator::readByBtn()
+{
+	QByteArray read_data;
+	while (sock.bytesAvailable())
+		read_data += sock.read(sock.bytesAvailable());
+
+	qDebug() << read_data;
+	log_msg(read_data);
 }
--- MKI_imitator/MKI_imitator.h
+++ MKI_imitator/MKI_imitator.h
@@ -18,6 +18,73 @@
 #include "../buses_imitator/frame_bus_rpc.h"
 #include "rpc_ports.h"
 
+/**
+*	\class таймер
+*
+*/
+class MyTimer
+{
+public:
+	/**
+	*	\brief конструктор
+	*
+	*/
+	MyTimer() : CounterStart(0)
+	{
+		StartCounter();
+		Sleep(1);
+		LARGE_INTEGER li;
+		QueryPerformanceCounter(&li);
+		Freq_In_mSec = li.QuadPart - CounterStart;
+	}
+	/**
+	*	\brief запуск счетчика
+	*
+	*/
+	bool StartCounter()
+	{
+		LARGE_INTEGER li;
+		if (!QueryPerformanceFrequency(&li))
+			return false;
+
+		QueryPerformanceCounter(&li);
+		CounterStart = li.QuadPart;
+		return true;
+	}
+	/**
+	*	\brief остановка счетчика
+	*
+	*/
+	void StopCounter()
+	{
+		LARGE_INTEGER li;
+		QueryPerformanceCounter(&li);
+		CounterStop = li.QuadPart;
+	}
+	/**
+	*	\brief получить секунды
+	*
+	*/
+	long GetSec()
+	{
+		return static_cast<long>(
+			std::ceil(double(CounterStop - CounterStart) / (Freq_In_mSec * 1000.0))
+			);
+	}
+	/**
+	*	\brief получить секунды
+	*
+	*/
+	long GetUSec()
+	{
+		return static_cast<long>((CounterStop - CounterStart) * 1000 / Freq_In_mSec);
+	}
+private:
+	__int64 CounterStart;
+	__int64 CounterStop;
+	__int64 Freq_In_mSec;
+};
+
 class MKI_imitator :
 	public QMainWindow
 {
@@ -29,11 +96,9 @@ public:
 private:
 	QWidget* widg;
 	
+	QTcpServer *tcpServer;
+	QTcpSocket* clientSocket;
 
-	QUdpSocket _sock;
-	QTcpSocket* _ag_sock = nullptr;
-	QTcpServer serv_sock;
-	
 	QLabel *control_label;
 	QLabel *rezhim_label;
 	QLabel *ip_label;
@@ -46,6 +111,102 @@ private:
 	
 	RPC_frame_bus_SLOT_Thread frame_slot_thr;
 	RPC_frame_bus_SIGNAL_Thread frame_signal_thr;
+
+	void log_msg(QString msg);
+	QPushButton *btn;
+	QTcpSocket sock;
+
+	enum control : unsigned int
+	{
+		MU = 1,
+		CU = 2
+	};
+
+	enum rezhim : int
+	{
+		RR = 1,
+		PBR = 2,
+		BR = 3,
+	};
+
+	enum RPIK : unsigned int
+	{
+		OFF = 0,
+		VTF = 1,
+		IK8 = 2,
+		IK15 = 3
+	};
+	int RUValue;
+	int RRValue;
+	int RPIKValue;
+	QString ipValue;
+	
+	const int msgHeaderLen = 96;
+	const int header_addr_receiver_ind = 4;
+	const int header_addr_sender_ind = 8;
+	const int header_MsgTypeC_ind = 12;
+	const int header_msg_reg_n_ind = 16;
+	const int header_sec_time_ind = 20;
+	const int header_usec_time_ind = 24;
+	const int header_krp_timeout_ind = 28;
+	const int header_kro_timeout_ind = 32;
+	const int header_err_stat_ind = 40;
+	const int header_LinkID_ind = 48;
+	const int header_linkStat_ind = 52;
+	const int header_datasize_ind = 56;
+	const int header_RU = 60;
+	const int header_RR = 64;
+	const int header_count_abonents_MTUA = 68;
+	const int header_count_abonents_MVI = 72;
+	const int header_ust_rezh_PKU = 76;
+	const int header_RPIK = 80;
+	const int header_number_KA = 84;
+	const int header_undefined = 88;
+	const int header_crc_header_ind = 92;
+	int MsgRegN = 0;
+	MyTimer my_timer;
+
+	QByteArray createCommandHeader(const int& command, const int& dataSize, int id = 0);
+
+	template <typename T>
+	/**
+	*	\brief целое в БА
+	* \param ba - массив БА
+	* \param data - данные
+	* \param start_ind - начальный индекс
+	*/
+	static void numberIntoBA(QByteArray& ba, T data, const int& start_ind)
+	{
+		int count = sizeof(T);
+
+		if (start_ind + count > ba.size())
+			ba.resize(start_ind + count);
+
+		for (int i = 0; i < count; ++i)
+		{
+			ba[start_ind + i] = (unsigned char)(data);
+			data = data >> 8;
+		}
+	}
+
+	template <typename T>
+	/**
+	*	\brief номер из БА
+	* \param ba - массив БА
+	* \param start_ind - начальный индекс
+	* \param bytes_count - количество байт
+	*/
+	static T numberFromBA(const QByteArray& ba, const int& start_ind, const int bytes_count = 0)
+	{
+		QDataStream tmp(ba);
+		tmp.setByteOrder(QDataStream::LittleEndian);
+		tmp.skipRawData(start_ind);
+
+		T data = 0;
+		tmp >> data;
+		return data;
+	}
+
 protected:
 	void closeEvent(QCloseEvent *event);
 signals:
@@ -54,10 +215,11 @@ signals:
 public slots :
 	void error_Slot(QAbstractSocket::SocketError socketError);
 	void read();
-	void connect_ag();
-	void read_ag();
 	void new_frame_07(QString mode_in, int psp_in, int lit_in, int _fm, QString _ant, QVariant frame_data);
-
+	void newConn();
+	void slotReadClient();
+	void writeByBtn();
+	void readByBtn();
 };
 
 #endif
