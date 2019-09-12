8bce3e9d7cb95687dbb0535fe82ef84f856d7144
vasya <lubchenko@sokb44.org>
2019-09-12 15:41:38 +0300

    Формирование и отправка массива из БУПа в ЦБК

--- BOOP_imitator/BOOP.cpp
+++ BOOP_imitator/BOOP.cpp
@@ -75,67 +75,7 @@ union BOOPRespondWord
 	};
 };
 
-union BOOPDataWords
-{
-	BOOPDataWords()
-	{
-		// Form data words
-	}
 
-	quint16 data_words[13];
-
-	struct {
-		quint16 upsilon_servo_power_status : 1,
-				phi_servo_power_status : 1,
-				upsilon_angle_sensor_power_status : 1,
-				phi_angle_sensor_power_status : 1,
-				upsilon_channel_work_status : 1,
-				phi_channel_work_status : 1,
-				previous_message_error : 1,
-				half_set_engage : 1,
-				temperature : 8;
-
-		quint16 upsilon_pulse_amount : 15,
-				upsilon_rotation_direction : 1;
-
-		quint16 phi_pulse_amount : 15,
-				phi_rotation_direction : 1;
-
-		quint16 uplsilon_pulse_frequency : 9,
-				: 7;
-
-		quint16 phi_pulse_frequency : 9,
-				: 7;
-
-		quint16 upsilon_angle;
-
-		quint16 phi_angle;
-
-		quint16 upsilon_pulse_summ : 15,
-				usplison_summ_sign : 1;
-
-		quint16 phi_pulse_summ : 15,
-				phi_summ_sign : 1;
-
-		// Self-control bits
-		quint16 bit19 : 1,
-				bit18 : 1,
-				bit17 : 1,
-				bit16 : 1,
-				bit15 : 1,
-				bit14 : 1,
-				bit13 : 1,
-				bit12 : 1,
-				: 7,
-				bit4 : 1;
-
-		quint16 upsilon_min_angle;
-
-		quint16 phi_min_angle;
-
-		quint16 checksum;
-	};
-};
 
 BOOP::BOOP()
 {
@@ -149,7 +89,7 @@ BOOP::BOOP()
   QLabel *temperatureLabel1 = new QLabel("Температура ", this);
   QLabel *temperatureLabel2 = new QLabel("°C", this);
 
-  temperatureEdit = new QLineEdit("-10", this);
+  temperatureEdit = new QLineEdit("20", this);
   temperatureEdit->setAlignment(Qt::AlignCenter);
   temperatureEdit->setFixedWidth(42);
 
@@ -423,61 +363,144 @@ void BOOP::new_message(QVariant dt, int MKO, int line, int command_word, QVarian
 	for (int i = 0; i < words.count() - 1; i++)
 		_checksum += words.at(i).toInt();
 
-	if (_checksum != words.last().toInt())
+	if (_checksum&0xFFFF != words.last().toInt())
+	{
+		word_for_cbk.previous_message_error = 1;
 		return;
-
-	MKODataWords parsed_data_words(words);
-
-	QString _message = QString("[%1] принял сигнал на подадресе %2 c КС %3")
-		                 .arg(QTime::currentTime().toString("hh:mm:ss.zzz"))
-										 .arg(parsed_command_word.subaddress).arg(parsed_command_word.command_word);
-
-	if (parsed_data_words.upsilon_servo_power)
-		upsilonAngleServoPower->setChecked(true);
-	else
-		upsilonAngleServoPower->setChecked(false);
-
-	if (parsed_data_words.phi_servo_power)
-		phiAngleServoPower->setChecked(true);
-	else
-		phiAngleServoPower->setChecked(false);
-
-	if (parsed_data_words.upsilon_angle_sensor_power)
-		upsilonAngleSensorPower->setChecked(true);
-	else
-		upsilonAngleSensorPower->setChecked(false);
-
-	if (parsed_data_words.phi_angle_sensor_power)
-		phiAngleSensorPower->setChecked(true);
-	else
-		phiAngleSensorPower->setChecked(false);
-
-	if (parsed_data_words.upsilon_rotation_command)
-	{
-		int _upsilon_current_angle = upsilonAngleValue->text().toInt(nullptr, 16);
-
-		if (parsed_data_words.upsilon_rotation_direction)
-			_upsilon_current_angle += parsed_data_words.upsilon_pulse_amount;
-		else
-			_upsilon_current_angle -= parsed_data_words.upsilon_pulse_amount;
-
-		upsilonAngleValue->setText(QString("%1").arg(_upsilon_current_angle, 0, 16).toUpper());
 	}
-
-	if (parsed_data_words.phi_rotation_command)
+	else
 	{
-		int _phi_current_angle = phiAngleValue->text().toInt(nullptr, 16);
 
-		if (parsed_data_words.phi_rotation_direction)
-			_phi_current_angle += parsed_data_words.phi_pulse_amount;
+		MKODataWords parsed_data_words(words);
+
+		QString _message = QString("[%1] принял сигнал на подадресе %2 c КС %3")
+			.arg(QTime::currentTime().toString("hh:mm:ss.zzz"))
+			.arg(parsed_command_word.subaddress).arg(parsed_command_word.command_word);
+
+		word_for_cbk.previous_message_error = 0;
+
+		if (parsed_data_words.upsilon_servo_power)
+		{
+			upsilonAngleServoPower->setChecked(true);
+			word_for_cbk.upsilon_servo_power_status = 1;
+		}
 		else
-			_phi_current_angle -= parsed_data_words.phi_pulse_amount;
+		{
+			upsilonAngleServoPower->setChecked(false);
+			word_for_cbk.upsilon_servo_power_status = 0;
+		}
+		if (parsed_data_words.phi_servo_power)
+		{
+			phiAngleServoPower->setChecked(true);
+			word_for_cbk.phi_servo_power_status = 1;
+		}
+		else
+		{
+			phiAngleServoPower->setChecked(false);
+			word_for_cbk.phi_servo_power_status = 1;
+		}
+		if (parsed_data_words.upsilon_angle_sensor_power)
+		{
+			upsilonAngleSensorPower->setChecked(true);
+			word_for_cbk.upsilon_angle_sensor_power_status = 1;
+		}
+		else
+		{
+			upsilonAngleSensorPower->setChecked(false);
+			word_for_cbk.upsilon_angle_sensor_power_status = 0;
+		}
+		if (parsed_data_words.phi_angle_sensor_power)
+		{
+			phiAngleSensorPower->setChecked(true);
+			word_for_cbk.phi_angle_sensor_power_status = 1;
+		}
+		else
+		{
+			phiAngleSensorPower->setChecked(false);
+			word_for_cbk.phi_angle_sensor_power_status = 0;
+		}
+		//тут еще должен быть признак ошибки по контрольной сумме
+		word_for_cbk.temperature = temperatureEdit->text().toInt(nullptr, 16);
 
-		phiAngleValue->setText(QString("%1").arg(_phi_current_angle, 0, 16).toUpper());
+		if (parsed_data_words.upsilon_rotation_command)
+		{
+			int _upsilon_current_angle = upsilonAngleValue->text().toInt(nullptr, 16);
+
+			word_for_cbk.upsilon_channel_work_status = 1;
+			word_for_cbk.upsilon_rotation_direction = parsed_data_words.upsilon_rotation_direction;
+			word_for_cbk.upsilon_pulse_amount = parsed_data_words.upsilon_pulse_amount; // уточнить у Олега
+
+			if (parsed_data_words.upsilon_rotation_direction)
+			{
+				_upsilon_current_angle += parsed_data_words.upsilon_pulse_amount;
+				word_for_cbk.upsilon_pulse_summ += parsed_data_words.upsilon_pulse_amount;// уточнить у Олега
+			}
+			else
+			{
+				_upsilon_current_angle -= parsed_data_words.upsilon_pulse_amount;
+				word_for_cbk.upsilon_pulse_summ -= parsed_data_words.upsilon_pulse_amount;// уточнить у Олега
+			}
+			word_for_cbk.upsilon_angle = _upsilon_current_angle;// уточнить у Олега
+			upsilonAngleValue->setText(QString("%1").arg(_upsilon_current_angle, 0, 16).toUpper());
+		}
+
+		if (parsed_data_words.phi_rotation_command)
+		{
+			int _phi_current_angle = phiAngleValue->text().toInt(nullptr, 16);
+
+			word_for_cbk.phi_channel_work_status = 1;
+			word_for_cbk.phi_rotation_direction = parsed_data_words.phi_rotation_direction;
+			word_for_cbk.phi_pulse_amount++; // уточнить у Олега
+
+			if (parsed_data_words.phi_rotation_direction)
+			{
+				_phi_current_angle += parsed_data_words.phi_pulse_amount;
+				word_for_cbk.phi_pulse_summ -= parsed_data_words.phi_pulse_amount;// уточнить у Олега
+			}
+			else
+			{
+				_phi_current_angle -= parsed_data_words.phi_pulse_amount;
+				word_for_cbk.phi_pulse_summ -= parsed_data_words.phi_pulse_amount;// уточнить у Олега
+			}
+
+			word_for_cbk.phi_angle = _phi_current_angle;
+			phiAngleValue->setText(QString("%1").arg(_phi_current_angle, 0, 16).toUpper());
+		}
+		word_for_cbk.uplsilon_pulse_frequency = parsed_data_words.uplsilon_pulse_frequency;
+		word_for_cbk.phi_pulse_frequency = parsed_data_words.phi_pulse_frequency;
+
+		(word_for_cbk.upsilon_pulse_summ > 0) ? word_for_cbk.usplison_summ_sign = 1 : word_for_cbk.usplison_summ_sign = 0;
+		(word_for_cbk.phi_pulse_summ > 0) ? word_for_cbk.phi_summ_sign = 1 : word_for_cbk.phi_summ_sign = 0;
+
+		//признаки самоконтроля БУП(Е)
+		bit19->isChecked() ? word_for_cbk.bit19 = 1 : word_for_cbk.bit19 = 0;
+		bit18->isChecked() ? word_for_cbk.bit18 = 1 : word_for_cbk.bit18 = 0;
+		bit17->isChecked() ? word_for_cbk.bit17 = 1 : word_for_cbk.bit17 = 0;
+		bit16->isChecked() ? word_for_cbk.bit16 = 1 : word_for_cbk.bit16 = 0;
+		bit15->isChecked() ? word_for_cbk.bit15 = 1 : word_for_cbk.bit15 = 0;
+		bit14->isChecked() ? word_for_cbk.bit14 = 1 : word_for_cbk.bit14 = 0;
+		bit12->isChecked() ? word_for_cbk.bit13 = 1 : word_for_cbk.bit12 = 0;
+		bit4->isChecked() ? word_for_cbk.bit4 = 1 : word_for_cbk.bit4 = 0;
+
+		//контрольная сумма
+		for (int i = 0; i++; i <= 13)
+			word_for_cbk.checksum += word_for_cbk.data_words[i];
+		word_for_cbk.checksum = word_for_cbk.checksum & 0xFFFF;
+
+		logArea->append(_message);
+		
+		new_tm();
 	}
-
-	logArea->append(_message);
 }
+
+void BOOP::new_tm()
+{
+
+	QVariantList tmp_list;
+	tmp_list.push_back(word_for_cbk.data_words[13]);
+	slot_thr.get_omnibus_obj()->set_new_data(MKO, address, 3, tmp_list);
+}
+
 void BOOP::new_matrix_command(int mshm, int pshm, int length_m, int length_p, double u_m, double u_p, int dt, int line_m, int line_p)
 {
 	if (pshm != 1 || mshm < 5 || mshm > 7)
@@ -491,28 +514,26 @@ void BOOP::new_matrix_command(int mshm, int pshm, int length_m, int length_p, do
 		slot_thr.get_omnibus_obj()->switch_ab(1, 9, true);
 		mainSetButton->setStyleSheet("background-color: rgb(142, 198, 156);");
 		reserveSetButton->setStyleSheet("background-color: rgb(204, 204, 204);");
+		halfset = 0;
 	}
 
 	if (mshm == 6) {
 		slot_thr.get_omnibus_obj()->switch_ab(1, 9, true);
 		reserveSetButton->setStyleSheet("background-color: rgb(142, 198, 156);");
 		mainSetButton->setStyleSheet("background-color: rgb(204, 204, 204);");
+		halfset = 1;
 	}
 
 	if (mshm == 7) {
 		slot_thr.get_omnibus_obj()->switch_ab(1, 9, false);
 		mainSetButton->setStyleSheet("background-color: rgb(204, 204, 204);");
 		reserveSetButton->setStyleSheet("background-color: rgb(204, 204, 204);");
+		halfset = 2;
 	}
 
 	logArea->append(_message);
 }
-void BOOP::new_data(int mko, int address, int subaddress, QVariantList words)
-{
 
-	// Do something
-
-}
 
 void BOOP::closeEvent(QCloseEvent *event)
 {
--- BOOP_imitator/BOOP.h
+++ BOOP_imitator/BOOP.h
@@ -12,6 +12,68 @@
 #include "../OMNIBUSBOX/omnibus_rpc.h"
 #include "../buses_imitator/mku_bus_rpc.h"
 
+union BOOPDataWords
+{
+	BOOPDataWords()
+	{
+		// Form data words
+	}
+
+	quint16 data_words[13];
+
+	struct {
+		quint16 upsilon_servo_power_status : 1,
+			phi_servo_power_status : 1,
+			upsilon_angle_sensor_power_status : 1,
+			phi_angle_sensor_power_status : 1,
+			upsilon_channel_work_status : 1,
+			phi_channel_work_status : 1,
+			previous_message_error : 1,
+			half_set_engage : 1,
+			temperature : 8;
+
+		quint16 upsilon_pulse_amount : 15,
+			upsilon_rotation_direction : 1;
+
+		quint16 phi_pulse_amount : 15,
+			phi_rotation_direction : 1;
+
+		quint16 uplsilon_pulse_frequency : 9,
+			: 7;
+
+		quint16 phi_pulse_frequency : 9,
+			: 7;
+
+		quint16 upsilon_angle;
+
+		quint16 phi_angle;
+
+		quint16 upsilon_pulse_summ : 15,
+			usplison_summ_sign : 1;
+
+		quint16 phi_pulse_summ : 15,
+			phi_summ_sign : 1;
+
+		// Self-control bits
+		quint16 bit19 : 1,
+			bit18 : 1,
+			bit17 : 1,
+			bit16 : 1,
+			bit15 : 1,
+			bit14 : 1,
+			bit13 : 1,
+			bit12 : 1,
+			: 7,
+			bit4 : 1;
+
+		quint16 upsilon_min_angle;
+
+		quint16 phi_min_angle;
+
+		quint16 checksum;
+	};
+};
+
 class BOOP : public QWidget
 {
 	Q_OBJECT
@@ -61,13 +123,21 @@ private:
 
 	// Log area
 	QTextEdit *logArea;
+	BOOPDataWords word_for_cbk;
+	int halfset = 2;
+	int upsilon_angl_amount;
+	int phi_angl_amount;
 protected:
 	void closeEvent(QCloseEvent *event);
 public slots:
 	void new_message(QVariant dt, int MKO, int line, int command_word, QVariantList words, int respond_word);
 	void new_matrix_command(int mshm, int pshm, int length_m, int length_p, double u_m, double u_p, int dt, int line_m, int line_p);
 
+signals:
+	
+
 private:
+	void new_tm();
 	RPC_omnibus_SLOT_Thread slot_thr;
 	RPC_omnibus_SIGNAL_Thread signal_thr;
 
@@ -76,6 +146,8 @@ private:
 
 	const int MKO = 1;
 	const int address = 9;
+
+
 };
 
 #endif
