32abde4f72e1f6253a6d363ae100d60736b33a44
oleg <okalinin@mail.ru>
2019-11-26 06:51:14 +0300

    rpc

--- LKA-05_imitator/lka05_rpc.cpp
+++ LKA-05_imitator/lka05_rpc.cpp
@@ -1,40 +1,5 @@
 #include "lka05_rpc.h"
 
-void RPC_lka05_SLOT_Object::connect_to_server()
-{
-	SRPCSignalClass::Instance().toLog(QString("lka05 slot connecting %1 %2").arg(addr).arg(port));
-	_sock = std::shared_ptr<QTcpSocket>(new QTcpSocket);
-	_sock->connectToHost(addr,port);
-	if (_sock->waitForConnected(3000))
-	{
-		connected = true;
-		SRPCSignalClass::Instance().toLog("lka05 slot connected");
-	}
-	else
-	{
-		connected = false;
-		SRPCSignalClass::Instance().toLog(QString("lka05 slot connection failed %1 %2").arg(_sock->error()).arg(_sock->errorString()));
-	}
-}
-
-void RPC_lka05_SIGNAL_Object::connect_to_server()
-{
-	SRPCSignalClass::Instance().toLog(QString("lka05 signal connecting %1 %2").arg(addr).arg(port));
-	_sock = std::shared_ptr<QTcpSocket>(new QTcpSocket);
-	_sock->connectToHost(addr,port);
-	if (_sock->waitForConnected(3000))
-	{
-		connected = true;
-		connect(_sock.get(), SIGNAL(readyRead()), this, SLOT(read_data()));
-		SRPCSignalClass::Instance().toLog("lka05 signal connected");
-	}
-	else
-	{
-		connected = false;
-		SRPCSignalClass::Instance().toLog(QString("lka05 signal connection failed %1 %2").arg(_sock->error()).arg(_sock->errorString()));
-	}
-}
-
 void RPC_lka05_SLOT_Thread::run()
 {
 	rpc_obj = std::shared_ptr<RPC_lka05_SLOT_Object>(new RPC_lka05_SLOT_Object(addr, port));
