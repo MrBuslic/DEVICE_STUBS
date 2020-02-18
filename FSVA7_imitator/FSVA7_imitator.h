fa92e66012ff7d1d090f981e24fba74dba567f83
energy <energy@sokb44.org>
2020-02-18 15:12:08 +0300

    FSVA7 имитатор

---
+++ FSVA7_imitator/FSVA7_imitator.h
@@ -0,0 +1,98 @@
+#ifndef FSVA7_IMITATOR_H
+#define FSVA7_IMITATOR_H
+
+#include <QMainWindow>
+#include <QPushButton>
+#include <QLayout>
+#include <QLabel>
+#include <QTextEdit>
+#include <QScrollBar>
+#include <QList>
+#include <QMessageBox>
+#include <QUDPSocket>
+#include <QTCPSocket>
+#include <QTcpServer>
+
+#include <boost/range/combine.hpp>
+#include <boost/tuple/tuple.hpp>
+#include <QGridLayout>
+
+class FSVA7_imitator :
+	public QMainWindow
+{
+	Q_OBJECT
+public:
+	FSVA7_imitator();
+	~FSVA7_imitator();
+	
+
+	double GetDELay() const;
+	void SetDELay();
+	double GetPositivePeak() const;
+	void SetPositivePeak();
+	int GetWCO() const;
+	void SetWCO();
+	int GetMKO_DATA() const;
+	void SetMKO_DATA();
+	double GetPEAK() const;
+	void SetPEAK();
+	double GetCURS1_VAL() const;
+	void SetCURS1_VAL();	
+	double GetCURS2_VAL() const;
+	void SetCURS2_VAL();
+private:
+	QWidget* widg;
+	
+	QUdpSocket _sock;
+	QTcpSocket* _ag_sock = nullptr;
+	QTcpServer serv_sock;
+
+	QGridLayout *gridlay;
+
+	QLabel DELay_label;
+	QTextEdit *DELay_edit;	//Значение задержки между включениями источников питания СИБП 
+	double DELay;
+
+	QLabel PositivePeak_label;
+	QTextEdit *PositivePeak_edit; // Пульсация напряжения помехи
+	double PositivePeak;
+
+	QLabel WCO_label;
+	QTextEdit *WCO_edit; // Количество слов на МКО
+	int WCO;
+
+	QLabel MKO_DATA_label;
+	QTextEdit *MKO_DATA_edit; // Слова на МКО
+	int MKO_DATA;
+
+	QLabel PEAK_label;
+	QTextEdit *PEAK_edit; // Значение напряжения на канале D1
+	double PEAK;
+
+	QLabel CURS1_VAL_label;
+	QTextEdit *CURS1_VAL_edit; // Разница во времени между двумя вертикальными линиями курсора 1
+	double CURS1_VAL;
+
+	QLabel CURS2_VAL_label;
+	QTextEdit *CURS2_VAL_edit; // Разница во времени между двумя вертикальными линиями курсора 2
+	double CURS2_VAL;
+
+
+protected:
+	void closeEvent(QCloseEvent *event);
+
+
+signals:
+
+
+public slots:
+	void error_Slot(QAbstractSocket::SocketError socketError);
+	void read();
+	void connect_ag();
+	void read_ag();
+
+
+};
+
+#endif
+
---
+++ FSVA7_imitator/main.cpp
@@ -0,0 +1,25 @@
+#include <QTextCodec>
+#include <QApplication>
+#include "FSVA7_imitator.h"
+#include <socket_rpc.h>
+
+#ifdef WIN32
+#include "windows.h"
+int __stdcall WinMain(HINSTANCE, HINSTANCE, LPSTR cmdLine, int)
+{
+	int argc = __argc;
+	char** argv = __argv;
+
+#else
+int main(int argc, char **argv)
+{
+#endif
+	QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+	QApplication app(argc, argv);	
+	LogWidget log_widget;
+	log_widget.show();
+	FSVA7_imitator FSVA7;
+	FSVA7.show();
+
+	return app.exec();
+}
