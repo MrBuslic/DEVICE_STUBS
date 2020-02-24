435496a9f03404b5ec67a91940e17692587c1487
energy <energy@sokb44.org>
2020-02-24 18:00:15 +0300

    Отладка имитатора FSVA7

--- FSVA7_imitator/FSVA7_imitator.h
+++ FSVA7_imitator/FSVA7_imitator.h
@@ -26,20 +26,11 @@ public:
 	~FSVA7_imitator();
 	
 
-	double GetDELay() const;
-	void SetDELay();
-	double GetPositivePeak() const;
-	void SetPositivePeak();
-	int GetWCO() const;
-	void SetWCO();
-	int GetMKO_DATA() const;
-	void SetMKO_DATA();
-	double GetPEAK() const;
-	void SetPEAK();
-	double GetCURS1_VAL() const;
-	void SetCURS1_VAL();	
-	double GetCURS2_VAL() const;
-	void SetCURS2_VAL();
+	/*double GetDELay() const;
+	void SetDELay();*/
+
+
+
 private:
 	QWidget* widg;
 	
@@ -49,33 +40,16 @@ private:
 
 	QGridLayout *gridlay;
 
-	QLabel DELay_label;
+	/*QLabel DELay_label;
 	QTextEdit *DELay_edit;	//Значение задержки между включениями источников питания СИБП 
-	double DELay;
+	double DELay;*/
 
-	QLabel PositivePeak_label;
-	QTextEdit *PositivePeak_edit; // Пульсация напряжения помехи
-	double PositivePeak;
+	int SPAN;
+	void setSPAN(int span);
 
-	QLabel WCO_label;
-	QTextEdit *WCO_edit; // Количество слов на МКО
-	int WCO;
+	int BWID;
+	void setBWID(int bwid);
 
-	QLabel MKO_DATA_label;
-	QTextEdit *MKO_DATA_edit; // Слова на МКО
-	int MKO_DATA;
-
-	QLabel PEAK_label;
-	QTextEdit *PEAK_edit; // Значение напряжения на канале D1
-	double PEAK;
-
-	QLabel CURS1_VAL_label;
-	QTextEdit *CURS1_VAL_edit; // Разница во времени между двумя вертикальными линиями курсора 1
-	double CURS1_VAL;
-
-	QLabel CURS2_VAL_label;
-	QTextEdit *CURS2_VAL_edit; // Разница во времени между двумя вертикальными линиями курсора 2
-	double CURS2_VAL;
 
 
 protected:
